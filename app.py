0d9f372a3c02469ad3c9a6d4f4c27e5f608229c8
Raúl Gandulfo <raulgandulfo@gmail.com>
2025-06-13 21:54:34 -0300

    Forzado de creación de tablas en app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from config import Config
 from models import db, Usuario
-from werkzeug.security import check_password_hash
-import create_admin
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,6 +10,20 @@ db.init_app(app)
 
 with app.app_context():
     db.create_all()
+
+    # Crear usuario admin si no existe
+    if not Usuario.query.filter_by(username='admin').first():
+        nuevo = Usuario(
+            nombre='Administrador',
+            username='admin',
+            password=generate_password_hash('admin123'),
+            rol='gerente'
+        )
+        db.session.add(nuevo)
+        db.session.commit()
+        print("✅ Usuario 'admin' creado")
+    else:
+        print("⚠️ Usuario 'admin' ya existe")
 
 @app.route('/')
 def index():
